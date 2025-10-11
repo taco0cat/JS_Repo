88f658b080448221914ce32b554c21ef83404219
taco0cat <hashirhameed107@gmail.com>
2025-10-11 18:36:05 +0400

    Check for increment & decrement

---
+++ 3 - Arithmetic Operators/index.js
@@ -0,0 +1,13 @@
+let students = 31;
+
+// students += 1;
+// students -= 1;
+// students *= 2;
+// students /= 2;
+// students **= 2;
+// let extraStudents = students % 3;
+
+students++; // increment by 1
+students--; // decrement by 1
+
+console.log(students);
