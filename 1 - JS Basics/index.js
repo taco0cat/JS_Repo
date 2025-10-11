a2c69ad4ae24d754fb66ff733514b66f7684a38b
taco0cat <hashirhameed107@gmail.com>
2025-10-11 12:53:34 +0400

    console.log, alert, getElementByID

--- 1 - JS Basics/index.js
+++ 1 - JS Basics/index.js
@@ -1,1 +1,13 @@
-console.log("Hello World")
+// "" or '' or ``
+// `` also for inserting variables later on 
+
+// Console Message
+    // console.log("Hello World");
+
+// Alert
+    // window.alert(`This is an alert!`);
+
+// Get Element by ID
+// Change text content with .textContent
+document.getElementById("myH1").textContent = "Hello";
+document.getElementById("myP").textContent = "I like pizza";
