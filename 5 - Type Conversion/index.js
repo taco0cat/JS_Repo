d5d36dce33a2112d43918d7387f321a87d6166e5
taco0cat <hashirhameed107@gmail.com>
2025-10-13 22:36:40 +0400

    Type Conversion

---
+++ 5 - Type Conversion/index.js
@@ -0,0 +1,19 @@
+// Data Type Conversion
+
+// let age = window.prompt("How old are you?");
+// age = Number(age); // Convert string  or boolean to number
+// age += 1;
+
+// console.log(age, typeof age);
+
+let x = "pizza";
+let y = "pizza";
+let z = ""; // if bool z is false, z is an empty string
+
+x = Number(x);
+y = String(y);
+z = Boolean(z);
+
+console.log(x, typeof x)
+console.log(y, typeof y)
+console.log(z, typeof z)
