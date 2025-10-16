12a04e8c21ecfda2077a98c6ded6f277ce7151bb
taco0cat <hashirhameed107@gmail.com>
2025-10-16 21:51:48 +0400

    FCC - Trivia Bot (Lab)

---
+++ 82 - FCC - Trivia Bot/index.js
@@ -0,0 +1,22 @@
+console.log("Hello! I'm your coding fun fact guide!")
+
+let botName = "bot1";
+let botLocation = "earth";
+let favoriteLanguage = "english";
+
+console.log(`My name is ` + botName + " and I live on " + botLocation + ".")
+
+console.log(`My favorite programming language is ` + favoriteLanguage + ".")
+
+let codingFact = "Fun Fact: " + favoriteLanguage;
+console.log(codingFact);
+
+codingFact = "None " + favoriteLanguage;
+
+console.log(codingFact);
+
+codingFact = "Useless " + favoriteLanguage;
+
+console.log(codingFact);
+
+console.log(`It was fun sharing these facts with you. Goodbye! - ${botName} from ${botLocation}.`)
