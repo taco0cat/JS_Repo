4b997514b0dd11efaf50803f139aa849ad0c67cc
taco0cat <hashirhameed107@gmail.com>
2025-10-16 22:00:34 +0400

    Concat / Add Strings

---
+++ 83- FCC - Sentence Maker/index.js
@@ -0,0 +1,22 @@
+let adjective, noun, verb, place, adjective2, noun2;
+
+adjective = "A"
+noun = "B"
+verb = "C"
+place = "D"
+adjective2 = "E"
+noun2 = "F"
+
+let firstStory = "Once upon a time, there was a(n) " +  adjective + " " +  noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + "."
+
+console.log("First story: " + firstStory)
+
+adjective = "G"
+noun = "H"
+verb = "I"
+place = "J"
+adjective2 = "K"
+noun2 = "L"
+
+let secondStory = "Once upon a time, there was a(n) " +  adjective + " " +  noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + "."
+console.log("Second story: " + secondStory)
