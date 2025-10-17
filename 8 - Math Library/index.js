f198d6efcfeb2c6247760886bf4dfccb5f06b2b3
taco0cat <hashirhameed107@gmail.com>
2025-10-17 20:55:47 +0400

    Math Library

---
+++ 8 - Math Library/index.js
@@ -0,0 +1,27 @@
+// Math Library
+
+// console.log(Math.PI); // PI
+// console.log(Math.E);  // Eulers Number
+
+let x = 3;
+let y = 2;
+let z = 1;
+
+// z = Math.round(x);
+// z = Math.floor(x);
+// z = Math.ceil(x);
+// z = Math.trunc(x);
+// z = Math.pow(x, y);
+// z = Math.sqrt(x);
+// z = Math.log(x);
+// z = Math.sin(x); // Radians
+// z = Math.cos(x); // Radians
+// z = Math.tan(x); // Radians
+// z = Math.abs(x);
+// z = Math.sign(x); // 1 if +ve, -1 if -ve, 0 if 0
+
+let max = Math.max(x, y, z);
+let min = Math.min(x, y, z);
+
+
+console.log(max)
