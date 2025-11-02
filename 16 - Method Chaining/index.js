c1c4b111b2e7fa23a48f87bb0317a966e68a0e64
taco0cat <hashirhameed107@gmail.com>
2025-11-02 21:05:20 +0400

    Method Chaining

---
+++ 16 - Method Chaining/index.js
@@ -0,0 +1,16 @@
+let username = window.prompt("Enter your username:");
+
+// No Chaining
+username = username.trim();
+let letter = username.charAt(0);
+letter = letter.toUpperCase();
+let extraChars = username.slice(1);
+extraChars = extraChars.toLowerCase()
+username = letter + extraChars
+// console.log(username) 
+
+
+// Chaining
+
+username = username.trim().charAt(0).toUpperCase() + username.trim().slice(1).toLowerCase()
+console.log(username)
