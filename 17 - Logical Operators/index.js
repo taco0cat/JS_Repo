2fc8ec98ff26e1c23333b74720ec9bcd8f1d6f79
taco0cat <hashirhameed107@gmail.com>
2025-11-03 00:56:10 +0400

    Logical Ops

---
+++ 17 - Logical Operators/index.js
@@ -0,0 +1,26 @@
+const temp1 = -100;
+
+if(temp1 > 0 && temp1 <= 30){
+    console.log("The weather is GOOD");
+}
+else{
+    console.log("The weather is BAD");
+}
+
+const temp2= -250;
+
+if(temp2<= 0 || temp2> 30){
+    console.log("The weather is BAD");
+}
+else{
+    console.log("The weather is GOOD");
+}
+
+const isSunny = true;
+
+if(!isSunny){
+    console.log("It is CLOUDY");
+}
+else{
+    console.log("It is SUNNY");
+}
