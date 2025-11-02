d019d484dee48898808402f38661acf7d29a4e70
taco0cat <hashirhameed107@gmail.com>
2025-11-03 00:59:45 +0400

    Strict Equality

---
+++ 18 - Strict Equality/index.js
@@ -0,0 +1,12 @@
+// = assignment
+// == comparison
+// === strict equality (same value & dtype)
+
+const PI = 3.14;
+
+if(PI === "3.14"){
+    console.log("That is NOT Pi");
+}
+else{
+    console.log("That is Pi");
+}
