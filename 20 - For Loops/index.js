cf40671323e89492db9ce1e1dee52c630befa0b0
taco0cat <hashirhameed107@gmail.com>
2025-11-03 01:13:43 +0400

    For Loops

---
+++ 20 - For Loops/index.js
@@ -0,0 +1,14 @@
+// For Loops
+
+for(let i = 1; i <= 10; i++){
+    if (i == 7){
+        continue;
+    }
+    else{
+        console.log(i)
+    }
+}
+
+for(let i = 10; i > 0; i--){
+    console.log(i);
+}
