2b4f1fa7a3b5103028b66bb9d83a1b119bccb62e
taco0cat <hashirhameed107@gmail.com>
2025-11-03 23:53:28 +0400

    Number Guesser

---
+++ 21 - Number Guessing Game/index.js
@@ -0,0 +1,7 @@
+// Number Guessing Game
+
+const minNum = 1;
+const maxNum = 100;
+const answer = Math.floor(Math.random() * (maxNum - Math.random() * (maxNum - minNum + 1) + minNum);
+
+console.log(answer);
